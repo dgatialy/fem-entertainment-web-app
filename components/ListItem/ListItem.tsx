7bb6f9871c39da5ea097a53ba486fbc8349f0da5
dgatialy <89516674+dgatialy@users.noreply.github.com>
2022-11-16 18:21:33 +0100

    Update ListItem.tsx

--- components/ListItem/ListItem.tsx
+++ components/ListItem/ListItem.tsx
@@ -4,7 +4,17 @@ import { toggleBookmarkAtom } from '../../utils/store';
 import type { Media } from '../../utils/store';
 import { useAtom } from 'jotai';
 
-export default function ListItem({ title, isBookmarked }: Media) {
+type Props = {
+    title: string,
+    year: number,
+    category: string,
+    rating: string,
+    isBookmarked: boolean,
+    isTrending: boolean,
+    image: string
+}
+
+export default function ListItem({ title, year, category, rating, isBookmarked, image }: Props) {
   const [, toggleTodo] = useAtom(toggleBookmarkAtom);
 
   return (
