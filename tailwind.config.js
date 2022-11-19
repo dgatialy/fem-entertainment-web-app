cab5b4d5e44dc7326c99ad8614b44eff0aaa81cc
dgatialy <89516674+dgatialy@users.noreply.github.com>
2022-11-19 07:24:03 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,11 @@
 
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
+    "./icons/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
