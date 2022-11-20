f5ce98f4370d29ead21fa4a6a2f4b9b26b303d6d
dgatialy <89516674+dgatialy@users.noreply.github.com>
2022-11-20 12:32:18 +0100

    Update Search.tsx

--- components/Search.tsx
+++ components/Search.tsx
@@ -49,7 +49,7 @@ export default function Search() {
 
   useEffect(() => {
     setSearch(false);
-  }, [pathname]);
+  }, [pathname, setSearch]);
 
   useEffect(() => {
     const keyDownHandler = (event: globalThis.KeyboardEvent) => {
