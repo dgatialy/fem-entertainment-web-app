6858b6896ed3eb318a1e80f5d0b2ef7b478bfc12
dgatialy <89516674+dgatialy@users.noreply.github.com>
2022-11-20 11:53:19 +0100

    Add loading state with skeleton

--- app/loading.js
+++ app/loading.js
@@ -1,4 +1,23 @@
+import GridLayout from "../layouts/GridLayout";
+import ContainerLayout from "../layouts/ContainerLayout";
+
 export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return <div>Loading ...</div>;
+  const content = Array.from({ length: 12 }, (v, i) => (
+    <div key={i} className="flex flex-col gap-2">
+      <div className="rounded bg-semi-dark-blue aspect-[140/87]"></div>
+      <div className="flex flex-col gap-2">
+        <div className="h-5 w-3/5 rounded bg-semi-dark-blue"></div>
+        <div className="h-7 w-4/5 rounded bg-semi-dark-blue"></div>
+      </div>
+    </div>
+  ));
+
+  return (
+    <ContainerLayout>
+      <div className="animate-pulse">
+        <div className="h-6 max-w-sm rounded bg-semi-dark-blue mt-12 mb-10"></div>
+        <GridLayout>{content}</GridLayout>
+      </div>
+    </ContainerLayout>
+  );
 }
