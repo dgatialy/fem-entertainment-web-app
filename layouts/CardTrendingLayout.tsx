27566ce158fced3dbff740a71af5b06b9cfbac36
dgatialy <89516674+dgatialy@users.noreply.github.com>
2022-11-20 20:12:16 +0100

    Update CardTrendingLayout.tsx

--- layouts/CardTrendingLayout.tsx
+++ layouts/CardTrendingLayout.tsx
@@ -17,17 +17,20 @@ export default function GridLayout({
   className?: string;
 }) {
   return (
-    <div className={`flex flex-col gap-2 ${className ? className : ""}`}>
+    <div
+      className={`group/card cursor-pointer flex flex-col gap-1.5 relative ${className}`}
+    >
       {thumbnail}
-      <div className="flex flex-col gap-1">
-        <div className="flex gap-2 text-xs sm:text-sm leading-tight font-light opacity-75">
+
+      <div className="absolute left-0 bottom-0 p-3 sm:p-6 flex flex-col gap-1 w-full bg-gradient-to-t from-black/75 to-blue-black/01 rounded-lg">
+        <div className="flex gap-2 text-xs sm:text-base leading-tight font-light opacity-75">
           <span className="flex gap-2 after:content-['·']">{year}</span>
           <span className="flex gap-2 after:content-['·']">
             <span className="flex gap-1.5 items-center">{category}</span>
           </span>
           <span>{rating}</span>
         </div>
-        <div className="font-medium text-sm sm:text-lg leading-tight">
+        <div className="font-medium text-sm sm:text-2xl leading-tight">
           {title}
         </div>
       </div>
